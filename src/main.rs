945a7fcf9e11a447ecde5fead0f568d9cf5e9727
kl4rk <w.lickefett@googlemail.com>
2023-05-06 17:22:05 +0200

    Fix spelling of test weight zero test

--- src/main.rs
+++ src/main.rs
@@ -106,7 +106,7 @@ mod test {
         assert_eq!(bmi_err, BmiError::HeightCannotBeZeroOrNegative);
     }
     #[test]
-    fn test_weight_zeros_err() {
+    fn test_weight_zero_err() {
         let bmi_err = calculate_bmi(Height(1.0), Weight(0.0)).unwrap_err();
         assert_eq!(bmi_err, BmiError::WeightCannotBeZeroOrNegative);
     }
